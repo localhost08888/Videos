38294842307bd6d67714c46619e6a30f7677a2b9
localhost08888 <nileshjadhav93078@gmail.com>
2024-12-20 15:40:46 +0530

    Update setc1.java

--- java/ass5/setc1.java
+++ java/ass5/setc1.java
@@ -1,6 +1,6 @@
 import java.io.File;
 
-class FileWatcher implements Runnable {
+class FileWatcher extends Thread {
     private final String fileName;
 
     public FileWatcher(String fileName) {
@@ -9,38 +9,32 @@ class FileWatcher implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Started watching file: " + fileName);
         while (true) {
             File file = new File(fileName);
             if (file.exists()) {
-                System.out.println("File found: " + fileName + ". Thread ending.");
-                break; // Exit the loop and terminate the thread
+                System.out.println("File found: " + fileName);
+                break; // Exit the thread
             } else {
                 System.out.println("File not found: " + fileName + ". Checking again in 5 seconds...");
-            }
-            try {
-                Thread.sleep(5000); // Wait for 5 seconds before checking again
-            } catch (InterruptedException e) {
-                System.out.println("Thread interrupted for file: " + fileName);
-                break;
+                try {
+                    Thread.sleep(5000); // Wait for 5 seconds
+                } catch (InterruptedException e) {
+                    System.out.println("Thread interrupted for file: " + fileName);
+                }
             }
         }
     }
 }
 
-public class setc1{
+public class FileWatcherDemo {
     public static void main(String[] args) {
-        // Example list of filenames to watch
-        String[] fileNames = {
-            "example1.txt",
-            "example2.txt",
-            "example3.txt"
-        };
+        // Array of file names to watch
+        String[] fileNames = {"file1.txt", "file2.txt", "file3.txt"};
 
-        // Create and start a thread for each file
+        // Start a thread for each file
         for (String fileName : fileNames) {
-            Thread thread = new Thread(new FileWatcher(fileName));
-            thread.start();
+            FileWatcher watcher = new FileWatcher(fileName);
+            watcher.start();
         }
     }
 }
