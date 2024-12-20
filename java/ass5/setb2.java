aabdef61bf84098f1a74841a61843ea1226e2eab
localhost08888 <nileshjadhav93078@gmail.com>
2024-12-20 15:38:09 +0530

    Update setb2.java

--- java/ass5/setb2.java
+++ java/ass5/setb2.java
@@ -1,163 +1,105 @@
-public import java.awt.*; 
-
-import java.awt.event.*; 
-
-  
-
-class Bouncing_Balls 
-
-    extends Frame implements MouseListener { 
-
-    // initializing co-ordinates 
-
-    int x = 40, y = 40, t1 = 1, t2 = 1; 
-
-    int x1 = 200, y1 = 40, t12 = 1, t22 = 1; 
-
-    int x2 = 100, y2 = 100, t13 = 1, t23 = 1; 
-
-    Thread th; 
-
-  
-
-    // Making constructor 
-
-    Bouncing_Balls() 
-
-    { 
-
-        setSize(700, 800); 
-
-        setVisible(true); 
-
-  
-
-        // Creating a new Thread 
-
-        th = new Thread(new Thread() { 
-
-            public void run() 
-
-            { 
-
-                while (true) { 
-
-                    x = x + t1; 
-
-                    y = y + t2; 
-
-                    x1 = x1 + t12; 
-
-                    y1 = y1 + t22; 
-
-                    x2 = x2 - t13; 
-
-                    y2 = y2 - t23; 
-
-  
-
-                    // specifying some condition to make 
-
-                    // balls move in a particular path 
-
-                    if (x < 0 || x > 680) 
-
-                        t1 = t1 * (-1); 
-
-                    if (y < 20 || y > 780) 
-
-                        t2 = t2 * (-1); 
-
-                    if (x1 < 0 || x1 > 680) 
-
-                        t12 = t12 * (-1); 
-
-                    if (y1 < 20 || y1 > 780) 
-
-                        t22 = t22 * (-1); 
-
-                    if (x2 < 0 || x2 > 680) 
-
-                        t13 = t13 * (-1); 
-
-                    if (y2 < 20 || y2 > 780) 
-
-                        t23 = t23 * (-1); 
-
-  
-
-                    try { 
-
-                        // Calling sleep method 
-
-                        this.sleep(5); 
-
-                    } 
-
-                    catch (Exception E) { 
-
-                    } 
-
-                    repaint(); 
-
-                } 
-
-            } 
-
-        }); 
-
-        addMouseListener(this); 
-
-    } 
-
-  
-
-    public void mouseClicked(MouseEvent M) 
-
-    { 
-
-        // Thread will start when mouse is clicked 
-
-        th.start(); 
-
-    } 
-
-    public void mousePressed(MouseEvent M) {} 
-
-    public void mouseReleased(MouseEvent M) {} 
-
-    public void mouseEntered(MouseEvent M) {} 
-
-    public void mouseExited(MouseEvent M) {} 
-
-  
-
-    public void paint(Graphics g) 
-
-    { 
-
-        g.setColor(Color.pink); 
-
-        g.fillOval(x, y, 40, 40); 
-
-        g.setColor(Color.pink); 
-
-        g.fillOval(x1, y1, 40, 40); 
-
-        g.setColor(Color.pink); 
-
-        g.fillOval(x2, y2, 40, 40); 
-
-    } 
-
-    public static void main(String[] args) 
-
-    { 
-
-        Bouncing_Balls B = new Bouncing_Balls(); 
-
-    } 
-
-} {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+class Ball extends Thread {
+    private int x, y, diameter;
+    private int speed;
+    private Color color;
+    private JPanel panel;
+    private boolean movingDown = true;
+
+    public Ball(JPanel panel) {
+        this.panel = panel;
+        Random random = new Random();
+
+        // Randomize initial position, speed, and color
+        this.x = random.nextInt(panel.getWidth() - 30); // Ensure the ball stays within the panel
+        this.y = random.nextInt(panel.getHeight() - 30);
+        this.diameter = 30;
+        this.speed = random.nextInt(5) + 2; // Speed between 2 and 6
+        this.color = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (panel) {
+                // Update position
+                if (movingDown) {
+                    y += speed;
+                    if (y + diameter >= panel.getHeight()) {
+                        movingDown = false; // Reverse direction
+                    }
+                } else {
+                    y -= speed;
+                    if (y <= 0) {
+                        movingDown = true; // Reverse direction
+                    }
+                }
+
+                // Repaint the panel to show updated position
+                panel.repaint();
+            }
+
+            // Delay for smoother animation
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x, y, diameter, diameter);
+    }
+}
+
+public class BallAnimation extends JFrame {
+    private JPanel panel;
+    private JButton startButton;
+    private ArrayList<Ball> balls;
+
+    public BallAnimation() {
+        setTitle("Ball Animation");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        // Panel to draw balls
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                // Draw all balls
+                for (Ball ball : balls) {
+                    ball.draw(g);
+                }
+            }
+        };
+        panel.setBackground(Color.WHITE);
+        add(panel, BorderLayout.CENTER);
+
+        // Start Button
+        startButton = new JButton("Start");
+        startButton.addActionListener(e -> {
+            Ball ball = new Ball(panel);
+            balls.add(ball);
+            ball.start(); // Start ball thread
+        });
+        add(startButton, BorderLayout.SOUTH);
+
+        balls = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            BallAnimation frame = new BallAnimation();
+            frame.setVisible(true);
+        });
+    }
 }
